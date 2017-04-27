701d6dd988a4c48d7e37132d9bfcc9ec1227a426
Saravudh Sinsomros <saravudh@shorsher.com>
2017-04-27 14:29:01 +0700

    F: แก้ Today ให้เป็นรูป

--- EPCalendar/EPCalendarPicker/SSCalendarPickerViewController.swift
+++ EPCalendar/EPCalendarPicker/SSCalendarPickerViewController.swift
@@ -126,7 +126,7 @@ open class SSCalendarPickerViewController: UIViewController, SSSelectionDateChan
         self.btnDone.tintColor = UIColor.white
         self.btnDoneCoverView.backgroundColor = EPDefaults.tintColor
         self.navigationController?.navigationBar.topItem?.title = ""
-        let todayButton = UIBarButtonItem(title: "Today", style: UIBarButtonItemStyle.plain, target: self.epCalendar, action:#selector(EPCalendarPicker.onTouchTodayButton))
+        let todayButton = UIBarButtonItem(image: UIImage(named: "today"), style: UIBarButtonItemStyle.plain, target: self.epCalendar, action: #selector(EPCalendarPicker.onTouchTodayButton))
         todayButton.tintColor = EPDefaults.todayTintColor
         self.navigationItem.rightBarButtonItem = todayButton
     }
